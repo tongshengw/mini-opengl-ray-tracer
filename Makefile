84aa40972c3a2ace48d615452b13489f5cce3cd8
tongshengw <trinityrushworth@gmail.com>
2024-12-19 18:04:14 -0800

    non-working .clangd file, working in vscode

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -I SDL2/x86_64-w64-mingw32/include -L SDL2/x86_64-w64-mingw32/lib -o main main.cpp -l mingw32 -l SDL2main -l SDL2
+	g++ -I SDL2/x86_64-w64-mingw32/include -L SDL2/x86_64-w64-mingw32/lib -o main main.cpp -l mingw32 -l SDL2main -l SDL2 --std=c++17 -Wall -Werror -pedantic -g
--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
 
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
 
 int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("test name", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+    if (!win) {
+        std::cout << "FAILED: create window" << SDL_GetError() << std::endl;
         return 1;
     }
 
