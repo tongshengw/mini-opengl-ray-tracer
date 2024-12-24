4293ee6e14a51afc5b320c4cd86f0b4960bee485
tongshengw <trinityrushworth@gmail.com>
2024-12-23 19:12:30 -0800

    triangle in opengl

---
+++ FragmentShader.glsl
@@ -0,0 +1,5 @@
+#version 410 core
+out vec4 color;
+void main() {
+    color = vec4(0.f, 0.f, 0.f, 1.f);
+}
--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include "SDL2/SDL_rect.h"
 #include "glad/glad.h"
 #include <SDL2/SDL.h>
-#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
-#include <cmath>
-#include <unordered_map>
 
-#include "LinearAlgebra.hpp"
 #include "SDL2/SDL_video.h"
-#include "Triangles.hpp"
 
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
@@ -25,7 +23,7 @@ public:
     std::vector<SDL_FPoint> points;
 
 public:
-    Screen() {
+    Screen(const std::string &vertexShaderIn, const std::string &fragmentShaderIn) {
         SDL_Init(SDL_INIT_EVERYTHING);
 
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
@@ -56,124 +54,95 @@ public:
         }
 
         printOpenGLInfo();
+
+        VertexSpecification();
+
+        CreateGraphicsPipeline(vertexShaderIn, fragmentShaderIn);
     }
 
     void PreDraw() {
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_CULL_FACE);
 
+        glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        glClearColor(1.f, 1.f, 0.f, 1.f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+        glUseProgram(graphicsPipelineShaders);
     }
     
     void Draw() {
+        glBindVertexArray(vertexArrayObject);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
+
     void add_point(float x, float y) {
         SDL_FPoint p{x, y};
         points.push_back(p);
     }
 
-    void render() {
-        // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        // SDL_RenderClear(renderer);
-        //
-        // Triangle test({100, 100, 0}, {500, 500, 0}, {100, 300, 0});
-        // drawTriangle(test);
-        //
-        // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        // for (SDL_FPoint p : points) {
-        //     SDL_RenderDrawPointF(renderer, p.x, p.y);
-        // }
-        //
-        // points.clear();
-        //
-        // SDL_RenderPresent(renderer);
-    } 
-    
 private:
-    void drawTriangle(Triangle t) {
-        std::unordered_map<int, std::pair<int, int>> edgePoints;
-        drawLine(edgePoints, t.a.x, t.a.y, t.b.x, t.b.y);
-        drawLine(edgePoints, t.b.x, t.b.y, t.c.x, t.c.y);
-        drawLine(edgePoints, t.a.x, t.a.y, t.c.x, t.c.y);
+    std::vector<GLfloat> tempTriangle{
+        0.2, 0.2, 0.f,
+        0.3, 0.1, 0.f,
+        0.1, 0.1, 0.f
+    };
 
-        std::unordered_map<int, std::pair<int, int>> throwaway;
-        for (std::pair<int, std::pair<int, int>> pair : edgePoints) {
-            if (pair.second.first != -1 && pair.second.second != -1) {
-                drawLine(throwaway, pair.first, pair.second.first, pair.first, pair.second.second);
-            }
-        }
+    // NOTE: OpenGL objects
+    GLuint vertexArrayObject = 0;
+    GLuint vertexBufferObject = 0;
+    GLuint graphicsPipelineShaders = 0;
+
+    
+    void VertexSpecification() {
+        glGenVertexArrays(1, &vertexArrayObject);
+        glBindVertexArray(vertexArrayObject);
+
+        glGenBuffers(1, &vertexBufferObject);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+        glBufferData(GL_ARRAY_BUFFER, tempTriangle.size() * sizeof(GLfloat), tempTriangle.data(), GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat), (void*)0);
+
+        glBindVertexArray(0);
+        glDisableVertexAttribArray(0);
     }
 
-    // NOTE: This function takes in 2 points and draws a line between these two points. This function also takes a unordered_map reference and pushes drawn points and their row to the map.
-    void drawLine(std::unordered_map<int, std::pair<int, int>> &edgePoints, float x1, float y1, float x2, float y2) {
-        if (std::abs(y2-y1) < std::abs(x2-x1)) {
-            if (x1 > x2) {
-                drawLineLow(edgePoints, x2, y2, x1, y1);
-            } else {
-                drawLineLow(edgePoints, x1, y1, x2, y2);
-            }
+    GLuint CompileShader(GLuint type, const std::string &src) {
+        GLuint shaderObject;
+        if (type == GL_VERTEX_SHADER) {
+            shaderObject = glCreateShader(GL_VERTEX_SHADER);
+        } else if (type == GL_FRAGMENT_SHADER) {
+            shaderObject = glCreateShader(GL_FRAGMENT_SHADER);
         } else {
-            if (y1 > y2) {
-                drawLineHigh(edgePoints, x2, y2, x1, y1);
-            } else {
-                drawLineHigh(edgePoints, x1, y1, x2, y2);
-            }
+            shaderObject = 0;
         }
+
+        const char* c_arr = src.c_str();
+        glShaderSource(shaderObject, 1, &c_arr, nullptr);
+        glCompileShader(shaderObject);
+
+        return shaderObject;
     }
 
-    void drawLineLow(std::unordered_map<int, std::pair<int, int>> &edgePoints, float x1, float y1, float x2, float y2) {
-        float dx = x2-x1;
-        float dy = y2-y1;
-        float yi = 1;
-        if (dy < 0) {
-            yi = -1;
-            dy = -dy;
-        }
-        float D = 2*dy - dx;
-        float y = y1;
+    void CreateGraphicsPipeline(const std::string &vertexShaderSrc, const std::string &fragmentShaderSrc) {
+        GLuint programObject = glCreateProgram();
+        GLuint loadedVertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderSrc);
+        GLuint loadedFragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSrc);
 
-        for (int i = (int) x1; i < x2; i++) {
-            add_point(i, y);
-            if (edgePoints.find(i) == edgePoints.end()) {
-                std::pair<int, int> p{y, -1};
-                edgePoints[i] = p;
-            } else {
-                edgePoints[i].second = y;
-            }
-            if (D>0) {
-                y = y+yi;
-                D = D + (2*(dy-dx));
-            } else {
-                D = D + 2*dy;
-            }
-        }
+        glAttachShader(programObject, loadedVertexShader);
+        glAttachShader(programObject, loadedFragmentShader);
+        glLinkProgram(programObject);
+        
+        glValidateProgram(programObject);
+
+        graphicsPipelineShaders = programObject;
     }
 
-    void drawLineHigh(std::unordered_map<int, std::pair<int, int>> &edgePoints, float x1, float y1, float x2, float y2) {
-        float dx = x2-x1;
-        float dy = y2-y1;
-        float xi = 1;
-        if (dx < 0) {
-            xi = -1;
-            dx = -dx;
-        }
-        float D = 2*dx - dy;
-        float x = x1;
 
-        for (int i = (int) y1; i < y2; i++) {
-            add_point(x, i);
-            if (edgePoints.find(x) == edgePoints.end()) {
-                std::pair<int, int> p{i, -1};
-                edgePoints[x] = p;
-            } else {
-                edgePoints[x].second = i;
-            }
-            if (D>0) {
-                x = x+xi;
-                D = D + (2*(dx-dy));
-            } else {
-                D = D + 2*dx;
-            }
-        }
-    }
 
     void printOpenGLInfo() {
         std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
@@ -184,8 +153,22 @@ private:
 
 
 int main(int argc, char** argv){
-    
-    Screen screen;
+    std::ifstream vs("VertexShader.glsl");
+    std::stringstream vsSS;
+    std::ifstream fs("FragmentShader.glsl");
+    std::stringstream fsSS;
+    if (vs && fs) {
+        vsSS << vs.rdbuf();
+        fsSS << fs.rdbuf();
+        vs.close();
+        fs.close();
+    } else {
+        std::cout << "FAILED: reading shaders" << std::endl;
+        class fileReadError{};
+        throw fileReadError{};
+    }
+
+    Screen screen(vsSS.str(), fsSS.str());
 
     // NOTE: Main loop
     bool running = true;
